55d9d16c39b7d7b9e8ad857a8940ba64f346897a
Frank Kachinga <frank.kachinga@gmail.com>
2022-06-08 11:18:20 +0300

    dev

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  server: 'http://localhost:8080',
-  /*server: 'https://mwengeparish.co.tz:8080',*/
+  /*server: 'http://localhost:8080',*/
+  server: 'https://mwengeparish.co.tz:8080',
 };
