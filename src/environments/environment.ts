10d8814f27c150f4b813f7cddd8a558f27b7e972
Frank Kachinga <frank.kachinga@gmail.com>
2023-04-18 14:40:00 +0300

    dv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   api: config.server + '/api/v1',
   page: 0,
   size: 5,
-  perPageOptions: [5, 20, 25, 50, 100, 200],
-  maxSize: 5
+  maxSize: 5,
+  perPageOptions: [5, 20, 25, 50, 100, 200]
 };
